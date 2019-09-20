9f363367ea9e364835616ce2b68f7b92128bd505
lukes8 <gw.lusi@seznam.cz>
2019-09-20 17:54:06 +0200

    Updated config env about external env file - better access for update Reorganized angular app architecture Added div with option to be called direct from gui

---
+++ src/main/resources/static/env.js
@@ -0,0 +1,8 @@
+(function(window) {
+    window.__env = window.__env || {};
+    
+    // API URL
+    window.__env.apiUrl = 'http://localhost:8080'; 
+    // window.__env.apiUrl= 'https://quiz-app-heroku.herokuapp.com';
+    window.__env.enableDebug = true;
+  }(this));
