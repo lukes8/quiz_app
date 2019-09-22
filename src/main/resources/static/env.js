7fad40eb27f9340e8f76e7889444dc1f8c33bb46
lukes8 <gw.lusi@seznam.cz>
2019-09-22 15:03:02 +0200

    Changed base API url for prod

--- src/main/resources/static/env.js
+++ src/main/resources/static/env.js
@@ -2,7 +2,7 @@
     window.__env = window.__env || {};
     
     // API URL
-    window.__env.apiUrl = 'http://localhost:8080'; 
-    // window.__env.apiUrl= 'https://quiz-app-heroku.herokuapp.com';
+//    window.__env.apiUrl = 'http://localhost:8080';
+     window.__env.apiUrl= 'https://quiz-app-heroku.herokuapp.com';
     window.__env.enableDebug = true;
   }(this));
