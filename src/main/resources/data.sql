be660c24186d7b6b9185a2e00fdd782e4926aafa
lukes8 <gw.lusi@seznam.cz>
2020-01-04 15:37:01 +0100

    Added category data

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -3,6 +3,10 @@ insert into quiz_category_entry (id, name, description) values (1, 'History', ''
 insert into quiz_category_entry (id, name, description) values (2, 'Geography', '');
 insert into quiz_category_entry (id, name, description) values (3, 'Nature', '');
 insert into quiz_category_entry (id, name, description) values (4, 'Languages', '');
+insert into quiz_category_entry (id, name, description) values (5, 'Autoskola', '');
+insert into quiz_category_entry (id, name, description) values (6, 'Others', '');
+insert into quiz_category_entry (id, name, description) values (7, 'Citaty', '');
+
 
 insert into quiz_question_entry (id, category_id, question, correct_answer, possible_answers, image_url) values (0, 1, 'Kdy prisli Cyril a Metodej na Moravu?', '863', '863;815;653', 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/c6/Cyril-methodius-small.jpg/215px-Cyril-methodius-small.jpg');
 --insert into quiz_question_entry (id, category_id, question, correctAnswer, possibleAnswers) values (1, 1, '');
